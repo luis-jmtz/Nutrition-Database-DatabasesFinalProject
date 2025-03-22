b6b3ab2436e97dbee2077041b5d5efe96dbb19a6
luis-jmtz <luis.jmtz2002@gmail.com>
2025-03-22 14:58:59 -0700

    placed db in the main folder of the project instead and renamed it to "nutrition.db"

--- sql_connection.py
+++ sql_connection.py
@@ -1,9 +1,6 @@
 import sqlite3
 
-# File path to your SQLite database
-db_path = "C:\\Users\\Main\\Desktop\\Profortilo Projects\\Nutrition-Database-DatabasesFinalProject\\database\\one.db"
-# Connect to the SQLite database (creates the file if it doesn't exist)
-conn = sqlite3.connect(db_path)
 
-# Create a cursor object to interact with the database
-cursor = conn.cursor()
+connection = sqlite3.connect("nutrition.db")
+
+
