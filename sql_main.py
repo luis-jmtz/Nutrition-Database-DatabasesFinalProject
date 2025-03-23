00f6e3a16b356076a815481fd09ea6def24ece1d
Luis-backup <ljmtz2002@gmail.com>
2025-03-22 21:16:04 -0700

    It looks like it worked

--- sql_main.py
+++ sql_main.py
@@ -5,14 +5,9 @@ connection = sqlite3.connect("nutrition.db")
 
 cursor = connection.cursor()
 
-show_tables(cursor)
-
-cursor.execute("DROP TABLE IF EXISTS FoodItem;")
-
-show_tables(cursor)
 
 # created the SQL Table
-cursor.executescript(open(r'SQL_Commands\table_creation.sql').read())
+#cursor.executescript(open(r'SQL_Commands\table_creation.sql').read())
 
 show_tables(cursor)
 
