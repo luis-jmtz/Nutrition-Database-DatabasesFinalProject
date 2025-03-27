4f32442d4a8efe4b70e679299424267d704dad16
luis-jmtz <luis.jmtz2002@gmail.com>
2025-03-27 10:40:40 -0700

    successfully created ingredients table

--- sql_main.py
+++ sql_main.py
@@ -7,19 +7,12 @@ cursor = connection.cursor()
 
 show_tables(cursor)
 
-# view_table(cursor, "IngredientItem")
+view_table(cursor, "IngredientItem")
 
 
 
 # created the SQL Table
 # cursor.executescript(open(r'SQL_Commands\table_creation.sql').read())
-
-# drop table 
-connection.execute("DROP TABLE IngredientItem")
-
-connection.execute("DROP TABLE FoodItem")
-
-show_tables(cursor)
 
 
 connection.close()
