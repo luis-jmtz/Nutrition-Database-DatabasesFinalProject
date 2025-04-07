c320d37429b826450bfbb1146526796eb112314a
Luis-backup <ljmtz2002@gmail.com>
2025-04-07 15:51:59 -0700

    added list with all of the filterable columns

--- App/app.py
+++ App/app.py
@@ -10,6 +10,19 @@ connection = sqlite3.connect("nutrition.db")
 cursor = connection.cursor()
 
 
+# List of all the filterable columns from IngredientItem table
+filter_columns = [
+    'Calories_per_100g', 'Protein_g', 'Fat_g', 'SaturatedFats_g',
+    'MonounsaturatedFats_g', 'PolyunsaturatedFats_g', 'Carbohydrates_g',
+    'Sugars_g', 'DietaryFiber_g', 'Cholesterol_mg', 'Sodium_mg',
+    'Water_g', 'VitaminA_mg', 'VitaminB1_mg', 'VitaminB11_mg',
+    'VitaminB12_mg', 'VitaminB2_mg', 'VitaminB3_mg', 'VitaminB5_mg',
+    'VitaminB6_mg', 'VitaminC_mg', 'VitaminD_mg', 'VitaminE_mg',
+    'VitaminK_mg', 'Calcium_mg', 'Copper_mg', 'Iron_mg', 'Magnesium_mg',
+    'Manganese_mg', 'Phosphorus_mg', 'Potassium_mg', 'Selenium_mg',
+    'Zinc_mg', 'NutritionDensity'
+]
+
 
 # @app.route("/")
 # def hello():
